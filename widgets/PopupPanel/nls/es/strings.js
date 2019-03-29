7e64d3ab94832b62c3896f389c294f32a7087144
marroca13 <mr13mar@gmail.com>
2019-03-29 13:30:46 +0100

    Copied files
    docs wwwroot

---
+++ widgets/PopupPanel/nls/es/strings.js
@@ -0,0 +1,13 @@
+define({
+  "_widgetLabel": "Panel emergente",
+  "previous": "Entidad anterior",
+  "next": "Entidad siguiente",
+  "clear": "Limpiar",
+  "cleartip": "Limpiar todos los resultados",
+  "clearsel": "Limpiar selección",
+  "clearseltip": "Limpiar resultados de la selección",
+  "selectfeatures": "Selecciona alguna entidad del mapa",
+  "widgetversion": "Widget de la ventana emergente versión informativa",
+  "widgetverstr": "Version del Widget",
+  "wabversionmsg": "Widget diseñado con Web AppBuilder"
+});
